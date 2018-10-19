fd5eea7dfdc8f7a1c16c22b02b2753cd6a1822e4
liaojiebiao <liaojiebiao@icanmake.cn>
2018-10-19 14:38:53 +0800

    一键mongo服务

---
+++ mongodbService.bat
@@ -0,0 +1,23 @@
+﻿@echo off 
+ net session >nul 2>&1
+if not "%errorLevel%" == "0" (
+  echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+  echo UAC.ShellExecute "%~s0", "%*", "", "runas", 1 >> "%temp%\getadmin.vbs"
+  "%temp%\getadmin.vbs"
+  exit /b 2
+)
+set mongobin=%~dp0
+ 
+::在mongodb文件夹下建立data,log文件夹
+md  %mongobin%data
+md  %mongobin%log
+
+cd %mongobin%bin
+set path==%path%;%mongobin%bin
+ 
+mongod --install  --serviceName maiyajiadb  --serviceDisplayName maiyajiadb --dbpath "%mongobin%data" --logpath "%mongobin%log\MongoDB.log" 
+ 
+net start maiyajiadb
+ 
+@echo Install finish!
+pause
